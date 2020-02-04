2309a6bcdd60df20a44c4de573b0f2b88011eea8
Harsh Kumar <harshkumarvc@gmail.com>
2020-02-05 01:29:40 +0530

    user_purchases table sql script

---
+++ userPurchases.sql
@@ -0,0 +1,35 @@
+create table User_Purchases (
+	purchase_id INT,
+	user_id INT,
+	receipt_id INT,
+	product_id INT,
+	quantity INT,
+	discounts_id INT,
+	before_discount INT,
+	after_discount INT
+);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (38, 85, 31, 94, 3, 63, 83958, 82958);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (44, 61, 77, 94, 15, 44, 91175, 90175);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (74, 4, 58, 1, 25, 84, 53141, 52141);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (46, 38, 70, 96, 23, 98, 27506, 26506);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (65, 4, 5, 43, 4, 28, 93335, 92335);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (82, 24, 71, 29, 14, 72, 77120, 76120);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (12, 42, 79, 58, 14, 78, 10508, 9508);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (81, 32, 58, 6, 20, 85, 53255, 52255);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (64, 35, 41, 93, 4, 49, 3569, 2569);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (63, 72, 93, 21, 24, 55, 96347, 95347);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (42, 73, 19, 13, 5, 94, 30363, 29363);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (41, 43, 36, 27, 1, 89, 12732, 11732);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (88, 94, 43, 20, 19, 22, 8973, 7973);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (57, 70, 73, 1, 22, 36, 91796, 90796);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (9, 75, 88, 63, 6, 43, 59361, 58361);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (33, 53, 46, 71, 25, 26, 95926, 94926);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (56, 85, 73, 62, 9, 69, 82119, 81119);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (36, 76, 95, 36, 16, 74, 72637, 71637);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (76, 64, 53, 55, 24, 94, 98481, 97481);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (77, 4, 33, 70, 5, 94, 93083, 92083);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (27, 94, 26, 4, 15, 10, 64260, 63260);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (13, 37, 73, 84, 15, 79, 48114, 47114);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (48, 11, 9, 47, 21, 52, 63291, 62291);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (37, 69, 11, 69, 17, 58, 91484, 90484);
+insert into User_Purchases (purchase_id, user_id, receipt_id, product_id, quantity, discounts_id, before_discount, after_discount) values (99, 70, 71, 72, 3, 30, 33477, 32477);
