0da777b7d6c0769a48b0598acc3fcdf0579236e9
Harsh Kumar <harshkumarvc@gmail.com>
2020-02-05 01:56:16 +0530

    added inventory, inventory, map sql scripts

---
+++ inventory.sql
@@ -0,0 +1,20 @@
+create table inventory (
+	product_id INT,
+	product_name VARCHAR(50),
+	quantity INT
+);
+insert into inventory (product_id, product_name, quantity) values (474, 'Colgate Toothpaste', 7);
+insert into inventory (product_id, product_name, quantity) values (955, 'Minute Maid Orange Juice', 6);
+insert into inventory (product_id, product_name, quantity) values (137, 'Nivea Facewash', 5);
+insert into inventory (product_id, product_name, quantity) values (57, 'Doritoes', 1);
+insert into inventory (product_id, product_name, quantity) values (809, 'Reynolds\' Pen', 3);
+insert into inventory (product_id, product_name, quantity) values (545, 'Well Detergent', 7);
+insert into inventory (product_id, product_name, quantity) values (940, 'Hide and Seek', 10);
+insert into inventory (product_id, product_name, quantity) values (465, 'Haldiram Aloo Bhujia', 3);
+insert into inventory (product_id, product_name, quantity) values (232, 'Tomato', 8);
+insert into inventory (product_id, product_name, quantity) values (965, 'Classmate Notebook', 8);
+insert into inventory (product_id, product_name, quantity) values (720, 'Fog Deodrant', 8);
+insert into inventory (product_id, product_name, quantity) values (263, 'Apple', 8);
+insert into inventory (product_id, product_name, quantity) values (953, 'Lays Uncle Chips', 4);
+insert into inventory (product_id, product_name, quantity) values (486, 'Nestle Coffee', 1);
+insert into inventory (product_id, product_name, quantity) values (448, 'Classmate Gel Pen', 5);
---
+++ map.sql
@@ -0,0 +1,21 @@
+create table map (
+	product_name VARCHAR(50),
+	product_id INT,
+	location_aisle INT,
+	location_shelf INT
+);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Doritoes', 813, 2, 4);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Colgate Toothpaste', 217, 4, 7);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Classmate Notebook', 805, 2, 4);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Hide and Seek', 207, 5, 7);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Nivea Facewash', 374, 8, 5);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Lays Uncle Chips', 537, 7, 5);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Apple', 944, 8, 8);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Nestle Coffee', 495, 10, 7);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Tomato', 160, 6, 5);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Minute Maid Orange Juice', 7, 1, 5);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Pepsi', 196, 7, 3);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Hit', 286, 2, 1);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Reynolds\' Pen', 881, 7, 4);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Fog Deodrant', 872, 8, 10);
+insert into map (product_name, product_id, location_aisle, location_shelf) values ('Haldiram Aloo Bhujia', 845, 8, 8);
